ac3e7dec5288e43c71be6e33256cf2701fc19cca
Humphrey Uno <unohumphrey260@gmail.com>
2026-07-24 14:20:58 +0800

    Add vercel analytics component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Hero from "../sections/Hero";
 import Projects from "../sections/Projects";
 import Footer from "@/shared/Footer";
 import Header from "@/shared/Header";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         </div>
         <TableOfContents />
       </div>
+      <Analytics mode="development" />
       <Footer />
     </div>
   );
